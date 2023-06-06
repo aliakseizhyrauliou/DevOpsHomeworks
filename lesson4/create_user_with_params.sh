6192ab14d042f44a403e1c2a25344cead621fe21
alex <alex@UbuntuALex.myguest.virtualbox.org>
2023-06-06 23:54:42 +0300

    Done

--- lesson4/create_user_with_params.sh
+++ lesson4/create_user_with_params.sh
@@ -1,55 +1,100 @@
 #!/bin/bash
 
-if [[ $# -lt 4 ]]; then
-    echo "4 agrguments required: -user_name -home_folder -ssh_access -enable"
+default_home="/home"
+
+# Функция, которая создает пользователя
+create_user() {
+  local username="$1"
+  local home_folder="${2:-${default_home}/${username}}"  # Используем значение по умолчанию, если home_folder не передан
+  local ssh="$3"
+  local is_enable="$4"
+  
+  if [[ -z "$username" ]]; then
+    echo "Имя пользователя не указано"
+    return 1
+  fi
+  
+  sudo useradd -m -d "$home_folder" "$username"
+  sudo passwd "$username"
+  
+  echo "========================================"
+  echo "Пользователь $username создан с домашней папкой $home_folder"
+
+  if [[ $is_enable = "true" ]]; then
+    echo "Пользователь включен"
+  else 
+    echo "Пользователь отключен"
+  fi
+
+  if [[ $ssh = "true" ]]; then
+    echo "SSH доступ включен"
+  else 
+    echo "SSH доступ отключен"
+  fi
+}
+# Функция, которая читает имена пользователей из файла и создает пользователей
+readFromFileAndCreateUsers() {
+  local ssh="$2"
+  local is_enable="$3"
+
+  if [[ $# -lt 1 ]]; then 
+    echo "Укажите путь к файлу"
     exit 1
-fi
+  fi
+  
+  file_path="$1"
+  
+  if [[ ! -f "$file_path" ]]; then
+    echo "Файл не найден: $file_path"
+    exit 1
+  fi
+  
+  while IFS= read -r username; do
+    create_user "$username" "" "$ssh" "$is_enable"
+  done < "$file_path"
+}
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    -user_name=*)
-      user_name="${1#*=}"
-      ;;
-    -home_folder=*)
-      home_folder="${1#*=}"
-      ;;
-    -ssh_access=*)
-      if [[ "${1#*=}" != "true" && "${1#*=}" != "false" ]]; then
-        echo "Error: Incorrect value for -ssh_access: ${1#*=}. Correct arguments: true, false"
+
+
+# Проверка и анализ аргументов командной строки
+validate_arguments() {
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -user_name=*)
+        user_name="${1#*=}"
+        ;;
+      -home_folder=*)
+        home_folder="${1#*=}"
+        ;;
+      -ssh_access)
+        ssh_access="true"
+        ;;
+      -enable)
+        enable="true"
+        ;;
+      -user_names_file_path=*)
+        user_names_file_path="${1#*=}"
+        ;;
+      *)
+        echo "Ошибка: Некорректный аргумент: $1"
         exit 1
-      fi
-      ssh_access="${1#*=}"
-      ;;
-    -enable=*)
-      if [[ "${1#*=}" != "true" && "${1#*=}" != "false" ]]; then
-        echo "Error: Incorrect value for -enable: ${1#*=}. Correct arguments: true, false"
-        exit 1
-      fi
-      enable="${1#*=}"
-      ;;
-    *)
-      echo "Ошибка: Некорректный аргумент: $1"
-      exit 1
-      ;;
-  esac
-  shift
-done
+        ;;
+    esac
+    shift
+  done
+}
 
-sudo useradd -m -d $home_folder $user_name
 
-sudo passwd "$user_name"
+# Проверка правильности аргументов
+validate_arguments "$@" # передаем аргументы из командной строки
 
-echo "user $user_name was created with with $home_folder home folder"
-
-if [[ $enable = "true" ]]; then
-    echo "user enable"
-else 
-    echo "user disabled"
+# Если передан путь к txt файлу с именами пользователей
+if [[ ! -z "$user_names_file_path" ]]; then
+  readFromFileAndCreateUsers "$user_names_file_path" "$ssh_access" "$enable"
+else
+  # Если путь к файлу не передан, создаем одного пользователя
+  # Мы уже проверили валидность данных в функции validate_arguments
+  create_user "$user_name" "$home_folder" "$ssh_access" "$enable"
 fi
 
-if [[ $ssh_access = "true" ]]; then
-    echo "ssh access enable"
-else 
-    echo "ssh access disabled"
-fi
 
